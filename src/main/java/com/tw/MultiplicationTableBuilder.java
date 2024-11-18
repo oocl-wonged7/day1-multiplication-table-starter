73f4d57fbd8b13971f711275e67006eadfaf0c52
oocl-wonged7 <edmond.c.m.wong@oocl.com>
2024-11-18 16:34:04 +0800

    feat: complete isStartNumberNotBiggerThanEndNumber function

--- src/main/java/com/tw/MultiplicationTableBuilder.java
+++ src/main/java/com/tw/MultiplicationTableBuilder.java
@@ -4,14 +4,18 @@ public class MultiplicationTableBuilder {
 
     public static void main(String[] args) {
         MultiplicationTableBuilder builder = new MultiplicationTableBuilder();
-        int start = 2;
-        int end = 4;
-        String multiplicationTable = builder.build(start, end);
+        int startNumber = 2;
+        int endNumber = 4;
+        System.out.println("isStartNumberNotBiggerThanEndNumber: " + isStartNumberNotBiggerThanEndNumber(startNumber, endNumber));
 
-        System.out.println(multiplicationTable);
+//        System.out.println(multiplicationTable);
     }
 
-    public String build(int start, int end) {
-        return "";
+//    public String build(int start, int end) {
+//        return "";
+//    }
+
+    private static boolean isStartNumberNotBiggerThanEndNumber(int startNumber, int endNumber) {
+        return startNumber <= endNumber;
     }
 }
