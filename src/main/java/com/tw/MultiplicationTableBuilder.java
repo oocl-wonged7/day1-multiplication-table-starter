ddacc55d7ac37059550135d762a634f7b8ca2181
oocl-wonged7 <edmond.c.m.wong@oocl.com>
2024-11-18 16:36:25 +0800

    feat: complete isNumbersInRange2To1000 function

--- src/main/java/com/tw/MultiplicationTableBuilder.java
+++ src/main/java/com/tw/MultiplicationTableBuilder.java
@@ -6,8 +6,8 @@ public class MultiplicationTableBuilder {
         MultiplicationTableBuilder builder = new MultiplicationTableBuilder();
         int startNumber = 2;
         int endNumber = 4;
-        System.out.println("isStartNumberNotBiggerThanEndNumber: " + isStartNumberNotBiggerThanEndNumber(startNumber, endNumber));
-
+        System.out.println("isStartNumberNotBiggerThanEndNumber: " + builder.isStartNumberNotBiggerThanEndNumber(startNumber, endNumber));
+        System.out.println("isNumbersInRange2To1000: " + builder.isNumbersInRange2To1000(startNumber, endNumber));
 //        System.out.println(multiplicationTable);
     }
 
@@ -18,4 +18,9 @@ public class MultiplicationTableBuilder {
     private static boolean isStartNumberNotBiggerThanEndNumber(int startNumber, int endNumber) {
         return startNumber <= endNumber;
     }
+
+    private static boolean isNumbersInRange2To1000(int start, int end) {
+        return (start > 1 && start <= 1000) && (end > 1 && end <= 1000);
+    }
+
 }
